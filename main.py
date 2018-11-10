0b2c688b5f4411f3e6b6a32f5a99dc49f7b36268
Combeau, A <arnaud.combeau@gmail.com>
2018-11-10 12:03:22 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+NAME = "arnaudc"
+
+
+def say(message):
+    print(message)
+
+
+async def bot():
+    reader, writer = await asyncio.open_connection(
+        "irc.freenode.net",
+        6667,
+        ssl=False
+    )
+    writer.write(b"NICK a1721951255\r\n")
+    writer.write(b"USER arnaudc * * *\r\n")
+    while not reader.at_eof():
+        line = await reader.readline()
+        line = line.decode('utf-8')
+        if line:
+            print(line)
+            if "001" in line:
+                writer.write(b"JOIN #pycoin18\r\n")
+                writer.write(b"NAMES #pycoin18\r\n")
+
+
+async def main():
+    while True:
+        line = await loop.run_in_executor(None, input, "? ")
+        cmd, *args = line.split(" ")
+        if cmd:
+            if cmd.lower() == "say":
+                message = " ".join(args)
+                say(message)
+            else:
+                logging.error("Unknown command: %s", cmd)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    tasks = (main(), bot())
+    try:
+        loop.run_until_complete(asyncio.gather(*tasks))
+    except KeyboardInterrupt:
+        pass
